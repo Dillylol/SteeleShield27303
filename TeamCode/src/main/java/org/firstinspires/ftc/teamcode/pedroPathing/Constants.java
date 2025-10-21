23c4a39edca4c13961bdb822307174503c2821d4
Dillylol <dylen814@gmail.com>
2025-10-21 01:28:40 -0500

    pedrotime

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -30,23 +32,29 @@ public class Constants {
             .forwardTicksToInches(.001989436789)
             .strafeTicksToInches(.001989436789)
             .turnTicksToInches(.001989436789)
-            .leftPodY(2.5)
-            .rightPodY(-2.5)
+            .leftPodY(-2.5)
+            .rightPodY(2.5)
             .strafePodX(-2.5)
             .leftEncoder_HardwareMapName("lf")
             .rightEncoder_HardwareMapName("lr")
             .strafeEncoder_HardwareMapName("rf")
             .leftEncoderDirection(Encoder.FORWARD)
             .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.FORWARD)
+            .strafeEncoderDirection(Encoder.REVERSE)
             .IMU_HardwareMapName("imu")
             .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.UP));
 
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(6.123)
             .forwardZeroPowerAcceleration(-48.046454662933335)
-            .lateralZeroPowerAcceleration(-77.3308376085305975);
-
+            .lateralZeroPowerAcceleration(-77.3308376085305975)
+            .useSecondaryTranslationalPIDF(false)
+            .useSecondaryHeadingPIDF(false)
+            .useSecondaryDrivePIDF(false)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.18, 0, 0.0125, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(1.2, 0, 0.15, 0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.02,0.0,0.00015,0.6,0.01))
+            .centripetalScaling(0.0001);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
